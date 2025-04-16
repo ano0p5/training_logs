0bbcf96f48e65ca7c1aab272756637e7427c0b95
ano0p5 <an00pkumar.ake@gmail.com>
2025-04-16 10:38:59 +0530

    Initial commit with training logs

---
+++ 2025-04-15/fry foods/fryfoods_category.py
@@ -0,0 +1,20 @@
+import requests
+
+url = 'https://www.frysfood.com/'
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive',
+    'Pragma': 'no-cache',
+    'Upgrade-Insecure-Requests': '1',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'none',
+    'Sec-Fetch-User': '?1'
+}
+
+response = requests.get(url, headers=headers)
+print("Status Code:", response.status_code)
